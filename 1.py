7e30d2796e9506666e41a7d2567a69cf9384e551
Laxmi Yadav <91363406+yadavlaxmi@users.noreply.github.com>
2022-03-31 10:23:06 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,12 @@
+def sum(a,b):
+  return(a+b)
+def sub(a,b):
+  return(a-b)
+def multi(a,b):
+  return(a*b)
+def call(a,b):
+  print(sum(2,3))
+  print(sub(5,6))
+  print(multi(2,2))
+  print(a,b)
+call(5,6)
